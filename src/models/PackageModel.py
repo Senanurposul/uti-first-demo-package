7216f4b88a5b7d787b45779d2162a4b5bca80771
Senanurposul <posulsenanur@gmail.com>
2026-07-30 09:52:07 +0300

    feat: PackageModel update

--- src/models/PackageModel.py
+++ src/models/PackageModel.py
@@ -1,3 +1,10 @@
+from pydantic import Field, validator
+from typing import List, Optional, Union, Literal
+from sdks.novavision.src.base.model import Package, Image, Inputs, Configs, Outputs, Response, Request, Output, Input, Config
+
+
+
+
 class InputImageTwo(Input):
     name: Literal["inputImageTwo"] = "inputImageTwo"
     value: Union[List[Image], Image]
